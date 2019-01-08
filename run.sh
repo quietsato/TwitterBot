3110306e373b45f51d3b78667c0a9d03daa2f93d
lnsf <quietsato@gmail.com>
2019-01-08 20:51:57 +0900

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+export ACCESS_TOKEN="xxxxxxxxxx"
+export ACCESS_TOKEN_SECRET="xxxxxxxxxx"
+export CONSUMER_KEY="xxxxxxxxxx"
+export CONSUMER_KEY_SECRET="xxxxxxxxxx"
+export TWITTER_USER_NAME="xxxxxxxxxx"
+
+python3 ./bot.py
