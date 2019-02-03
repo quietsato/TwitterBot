f26c688ad1996fe051d859170e404f56dfb7e548
lnsf <lnsf03@gmail.com>
2019-02-03 14:29:52 +0900

    :construction: リファクタリングとか長いやつの除外

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 import json
 import os
 import sys
-from random import randint
-from pprint import pprint
+import random
+import math
 
 import MeCab as M
-import requests
 from requests_oauthlib import OAuth1Session
 
 ACCESS_TOKEN = None
@@ -15,66 +14,86 @@ CONSUMER_KEY_SECRET = None
 USER_NAME = None
 
 
-def main():
-    get_environ()
-    tweets = get_tweet()
-    chain_block = create_chain_block(tweets)
-    sentences = generate_sentences(chain_block)
-    sentence = choose_sentence(sentences)
-    fmt_sentence = format_sentence(sentence)
-    print(fmt_sentence)
-    tweet(fmt_sentence)
+def get_environ():
+    global ACCESS_TOKEN
+    global ACCESS_TOKEN_SECRET
+    global CONSUMER_KEY
+    global CONSUMER_KEY_SECRET
+    global USER_NAME
+
+    ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+    ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
+    CONSUMER_KEY = os.getenv('CONSUMER_KEY')
+    CONSUMER_KEY_SECRET = os.getenv('CONSUMER_KEY_SECRET')
+    USER_NAME = os.getenv('TWITTER_USER_NAME')
+
+    for env in [ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_KEY_SECRET]:
+        if env is None:
+            print('E: Environment value is not set.')
+            sys.exit(1)
 
 
 def get_tweet():
-    tweets = []
-    _id = 0
+    session = OAuth1Session(CONSUMER_KEY, CONSUMER_KEY_SECRET,
+                            ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-    req = access_twitter_api()
+    url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+    params = {'count': 200}
 
-    if req.status_code == 200:
-        timeline = json.loads(req.text)
+    res = session.get(url, params=params)
+
+    if res.status_code == 200:
+        timeline = json.loads(res.text)
+        tweets = []
+        tweet_id = 0
+
         for tweet in timeline:
             tw = ''
-            st = str(tweet['text'])
+            text = str(tweet['text'])
             user = str(tweet['user']['screen_name'])
 
-            if(st.startswith('@')):
+            if(text.startswith('@')):
                 # @リプライは除外する
                 continue
-            if(st.startswith('RT @')):
+            if(text.startswith('RT @')):
                 # リツイートは除外する
                 continue
-            if 'http' in st:
-                #URL付きツイートは外部サービス感が出るから除外
+            if 'http' in text:
+                # URL付きツイートは外部サービス感が出るから除外
                 continue
             if(user == USER_NAME):
                 # 自分のツイートから学習するとつまらない
                 continue
 
-            for w in st.split():
-                if '@' in w:
-                    # ツイート途中の@ユーザー名は除外する
-                    break 
-                if '#' in w:
+            for word in text.split():
+                if '@' in word:
+                    # ツイート途中に@ユーザー名が出てきたら飛ばす
+                    break
+                if '#' in word:
                     # ハッシュタグ以降は取り入れない
                     break
-                tw += ' ' + w
-            
+                tw += ' ' + word
+
             tw = tw.strip()
+
+
             if len(tw) > 0:
-                tweets.append((tw, _id))
-                _id += 1    
+                tweets.append((tw, tweet_id))
+                tweet_id += 1
+
+        return tweets
 
     else:
-        print('E: Could not get home timeline at error ' + str(req.status_code))
+        print('E: Could not get home timeline at error ' + str(res.status_code))
+        sys.exit(1)
 
     return tweets
 
-def create_chain_block(tweets):
+
+def create_tokenized_blocks(tweets):
     # MeCabのモードをスペース区切りに設定する
     m = M.Tagger('-Owakati')
-    chain_block = []
+    block = []
 
     for tweet in tweets:
         words = str(m.parse(tweet[0])).split()
@@ -86,81 +105,84 @@ def create_chain_block(tweets):
         _id = tweet[1]
 
         for i in range(len(words) - 2):
-            chain_block.append([words[i], words[i + 1], words[i + 2], _id])
+            block.append([words[i], words[i + 1], words[i + 2], _id])
 
-    return chain_block
+    return block
 
 
-def generate_sentences(chain_block):
-    sentences = []
+def join_blocks(blocks):
+    joined_blocks = []
 
     # 文頭となりえる要素を抽出
-    heads = [b for b in chain_block if b[0] == '__BEGIN__']
+    heads = [b for b in blocks if b[0] == '__BEGIN__']
 
     for head in heads:
-        sentence = []
-        blocks = chain_block
-        sentence.append(head)
+        joined = []
+        joined.append(head)
         while True:
             # 生成文の末尾が先頭に来るような要素を見つけて配列にする
-            block_available = [b for b in chain_block if b[0]
-                            == sentence[len(sentence) - 1][2]]
-            if len(block_available) == 0:
+            nominated_blocks = [b for b in blocks
+                                if b[0] == joined[len(joined) - 1][2]]
+
+            if len(nominated_blocks) == 0:
                 break
 
             # その中からランダムに文末にくっつける
-            block = block_available[randint(0, len(block_available) - 1)]
-            sentence.append(block)
+            block = random.choice(nominated_blocks)
+            joined.append(block)
 
-            blocks.remove(block)
-        
-        sentences.append(sentence)
+        joined_blocks.append(joined)
 
-    return sentences
+    return joined_blocks
 
-def choose_sentence(sentences):
+
+def select_block(joined_blocks):
     # 選ばれる候補
+    # tmp = []
     tmp = []
 
-    for st in sentences:
-        # 何回1つのツイートが連続して連結されたか
+    for joined in joined_blocks:
+        # 何回同じのツイートからとってきたブロックが連続して連結されたか
         cost = 0
-        for i in range(len(st) - 1):
-            if(st[i][3] == st[i + 1][3]):
+        for i in range(len(joined) - 1):
+            if (joined[i][3] == joined[i + 1][3]):
                 cost += 1
-        
-        # 左辺==1のとき、ツイートがそのまま吐き出されるので除く
-        # オリジナリティーが欲しいので2まで除いてやる
-        if (len(st) - cost > 2):
-            tmp.append(st)
-    
-    idx = randint(0,len(tmp))
 
-    sentence = tmp[idx]
+        # ツイートする組を選ぶ
+        print(cost)
+        if(len(joined) > 20):
+            continue
+        if (len(joined) - cost > math.floor(len(joined) * 0.5)):
+            tmp.append(joined)
 
-    return sentence
+    for t in tmp:
+        print(convert_blocks_tostr(t))
+    return random.choice(tmp)
 
 
-def format_sentence(sentence):
-    fmt_sentence = ''
+def convert_blocks_tostr(blocks):
+    string = ''
 
-    for block in sentence:
+    for block in blocks:
         for word in block[1:3]:
-            if(word != '__BEGIN__'):
-                # ハッシュタグを生かす
-                if(word.startswith('#')):
-                    fmt_sentence += ' '
-                fmt_sentence += word
+            if(word == '__BEGIN__'):
+                break
+            # ハッシュタグを生かす
+            if(word.startswith('#')):
+                string += ' '
+            if(word == '__END__'):
+                break
+            string += word
 
-    return fmt_sentence
+    return string
 
 
-def tweet(sentence):
+def tweet(text):
     twitter = OAuth1Session(CONSUMER_KEY, CONSUMER_KEY_SECRET,
                             ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
     url = 'https://api.twitter.com/1.1/statuses/update.json'
-    params = {'status': sentence}
+    params = {'status': text}
 
     res = twitter.post(url, params=params)
 
@@ -170,35 +192,12 @@ def tweet(sentence):
         print('E: Tweet failed at error: ' + str(res.status_code))
 
 
-def access_twitter_api():
-    twitter = OAuth1Session(CONSUMER_KEY, CONSUMER_KEY_SECRET,
-                            ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-
-    url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-    params = {'count': 200}
-
-    req = twitter.get(url, params=params)
-    return req
-
-
-def get_environ():
-    global ACCESS_TOKEN
-    global ACCESS_TOKEN_SECRET
-    global CONSUMER_KEY
-    global CONSUMER_KEY_SECRET
-    global USER_NAME
-
-    ACCESS_TOKEN = os.getenv('ACCESS_TOKEN', default=None)
-    ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET', default=None)
-    CONSUMER_KEY = os.getenv('CONSUMER_KEY', default=None)
-    CONSUMER_KEY_SECRET = os.getenv('CONSUMER_KEY_SECRET', default=None)
-    USER_NAME = os.getenv('TWITTER_USER_NAME', default=None)
-
-    for env in [ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_KEY_SECRET, USER_NAME]:
-        if env is None:
-            print('E: Environment value is not set.')
-            sys.exit(1)
-
-
 if __name__ == "__main__":
-    main()
+    get_environ()
+    tweets = get_tweet()
+    blocks = create_tokenized_blocks(tweets)
+    joined_blocks = join_blocks(blocks)
+    block = select_block(joined_blocks)
+    text = convert_blocks_tostr(block)
+    print(text)
+    # tweet(text)
