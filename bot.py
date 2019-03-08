cb1f4b0572769a1d91d884c245f57c48106fdb4b
lnsf <lnsf03@gmail.com>
2019-03-08 15:29:37 +0900

    Revert to stable

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 import sys
 import random
 import math
+import re
 
 import MeCab as M
 from requests_oauthlib import OAuth1Session
@@ -15,7 +16,10 @@ CONSUMER_KEY_SECRET = None
 USER_NAME = None
 
 verbose = False
-
+do_tweet = True
+ignore_path = None
+max_length = 20
+tweet_count = 200
 
 def get_environ():
     global ACCESS_TOKEN
@@ -34,9 +38,9 @@ def get_environ():
         if env is None:
             print('E: Environment value is not set.')
             sys.exit(1)
-
+    
     if verbose:
-        print('Your Twitter User Name is ' + USER_NAME + '\n')
+        print('Your Twitter User Name is ' + USER_NAME)
 
 
 def get_tweet():
@@ -44,7 +48,7 @@ def get_tweet():
                             ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
     url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-    params = {'count': 200}
+    params = {'count': tweet_count}
 
     res = session.get(url, params=params)
 
@@ -54,50 +58,78 @@ def get_tweet():
         tweet_id = 0
 
         if verbose:
-            print('Got Tweets From API (count: ' + str(len(timeline)) + ')\n')
+            print('Got Tweets From API (count: ' + str(len(timeline)) + ')')
+
+        ignores = [re.compile(i) for i in read_ignores()]
 
         for tweet in timeline:
             tw = ''
             text = str(tweet['text'])
             user = str(tweet['user']['screen_name'])
 
-            if(text.startswith('@')):
-                # @リプライは除外する
-                continue
-            if(text.startswith('RT @')):
-                # リツイートは除外する
-                continue
-            if 'http' in text:
-                # URL付きツイートは外部サービス感が出るから除外
-                continue
+            isignore = False
+
             if(user == USER_NAME):
                 # 自分のツイートから学習するとつまらない
                 continue
 
-            for word in text.split():
-                if '@' in word:
-                    # ツイート途中に@ユーザー名が出てきたらもう読まない
-                    break
-                if '#' in word:
-                    # ハッシュタグ以降は取り入れない
-                    break
-                tw += ' ' + word
+            for ig in ignores:
+                match = ig.match(text)
+                if match is None:
+                    continue
+                if verbose:
+                    print('Ignore:  ' + match.group())
+                if match.span()[0] == 0:
+                    text = ''
+                else:
+                    text = text[:match.span()[0]] + text[match.span()[1]:]
+
+            if isignore:
+                continue
+
+            tw += text
 
             tw = tw.strip()
+
 
             if len(tw) > 0:
                 tweets.append((tw, tweet_id))
                 tweet_id += 1
                 if verbose:
-                    print('Add: ' + tw)
+                    print('Add: ' + tw )
 
         if verbose:
-            print('Create Tweet List (count:' + str(len(tweets)) + ')\n')
+            print('Create Tweet List (count:' + str(len(tweets)) + ')')
         return tweets
 
     else:
         print('E: Could not get home timeline at error ' + str(res.status_code))
         sys.exit(1)
+
+
+def read_ignores():
+    default_ignores = [
+        r'[\s\S]*?(https?)[\s\S]*$',
+        r'RT @[\s\S]*$',
+        r'#[\s\S]*$',
+        r'@[\s\S]*$']
+
+    if ignore_path is None:
+        return default_ignores
+
+    try:
+        with open(ignore_path) as f:
+            j = json.loads(str(f.read()))
+            ignores = j['ignores']
+            if verbose:
+                print('Ignore Patterns:')
+                for i in ignores + default_ignores:
+                    print(i)
+            return ignores + default_ignores
+
+    except:
+        print('W: Error reading ignore config. Use default setting.')
+        return default_ignores
 
 
 def create_tokenized_blocks(tweets):
@@ -117,8 +149,6 @@ def create_tokenized_blocks(tweets):
         for i in range(len(words) - 2):
             block.append([words[i], words[i + 1], words[i + 2], _id])
 
-    if verbose:
-        print('Create Blocks (count: ' + str(len(block)) + ')\n')
     return block
 
 
@@ -133,7 +163,13 @@ def join_blocks(blocks):
         joined.append(head)
         while True:
             # 生成文の末尾が先頭に来るような要素を見つけて配列にする
-            nominated_blocks = [b for b in blocks if b[0] == joined[len(joined) - 1][2]]
+<<<<<<< HEAD
+            nominated_blocks = [b for b in blocks if b[0]
+                                == joined[len(joined) - 1][2]]
+=======
+            nominated_blocks = [b for b in blocks
+                                if b[0] == joined[len(joined) - 1][2]]
+>>>>>>> parent of 755aef2... :ambulance: "ほっんほっ"みたいな永遠に続きそうなツイートが来ると無限ループで帰ってこなくなるのを修正
 
             if len(nominated_blocks) == 0:
                 break
@@ -142,10 +178,13 @@ def join_blocks(blocks):
             block = random.choice(nominated_blocks)
             joined.append(block)
 
+<<<<<<< HEAD
             # 長すぎる文はカット
-            if(len(joined) > 20):
+            if(len(joined) > max_length):
                 break
 
+=======
+>>>>>>> parent of 755aef2... :ambulance: "ほっんほっ"みたいな永遠に続きそうなツイートが来ると無限ループで帰ってこなくなるのを修正
         joined_blocks.append(joined)
 
     return joined_blocks
@@ -153,6 +192,7 @@ def join_blocks(blocks):
 
 def select_block(joined_blocks):
     # 選ばれる候補
+    # tmp = []
     tmp = []
 
     for joined in joined_blocks:
@@ -163,6 +203,8 @@ def select_block(joined_blocks):
                 cost += 1
 
         # ツイートする組を選ぶ
+        if(len(joined) > 20):
+            continue
         if (len(joined) - cost > math.floor(len(joined) * 0.5)):
             tmp.append(joined)
 
@@ -170,12 +212,7 @@ def select_block(joined_blocks):
                 print('text: ' + convert_blocks_tostr(joined) + ' len: ' + str(len(joined)) +
                       ' cost: ' + str(cost))
 
-    choice = random.choice(tmp)
-
-    if verbose:
-        print('\nSelect: ' + convert_blocks_tostr(choice) + '\n')
-
-    return choice
+    return random.choice(tmp)
 
 
 def convert_blocks_tostr(blocks):
@@ -205,24 +242,57 @@ def tweet(text):
     res = twitter.post(url, params=params)
 
     if res.status_code == 200:
-        print('success!!')
+        if verbose:
+            print('Tweet success!!')
     else:
         print('E: Tweet failed at error: ' + str(res.status_code))
 
 
 def argment_parser():
-    usage = 'Usage: python {} [--verbose] [--help]'\
+    usage = 'Usage: python3 {} [-v] [-nt] [-i] FILE [-c] INT [-m] INT [--help]'\
             .format(__file__)
     argparser = ArgumentParser(usage=usage)
     argparser.add_argument('-v', '--verbose',
                            action='store_true',
                            help='show verbose message')
+<<<<<<< HEAD
+    argparser.add_argument('-nt', '--no-tweet',
+                           action='store_true',
+                           help='don\'t post tweet, just console output')
+    argparser.add_argument('-i', '--ignores',
+                           help='set ignores configration file',
+                           type=str)
+    argparser.add_argument('-c', '--get-count',
+                           help='count of tweets got from TimeLine, default is 200',
+                           type=int)
+    argparser.add_argument('-m', '--max-length',
+                           help='set max count of blocks to generate sentence, default is 20',
+                           type=int)
 
+=======
+    
+>>>>>>> parent of 755aef2... :ambulance: "ほっんほっ"みたいな永遠に続きそうなツイートが来ると無限ループで帰ってこなくなるのを修正
     args = argparser.parse_args()
 
     global verbose
-    verbose = args.verbose
+    global do_tweet
+    global ignore_path
+    global tweet_count
+    global max_length
 
+    verbose = args.verbose
+<<<<<<< HEAD
+    do_tweet = not args.no_tweet
+    if args.ignores:
+        ignore_path = args.ignores
+    if args.get_count:
+        tweet_count = args.get_count
+    if args.max_length:
+        max_length = args.max_length
+
+=======
+    
+>>>>>>> parent of 755aef2... :ambulance: "ほっんほっ"みたいな永遠に続きそうなツイートが来ると無限ループで帰ってこなくなるのを修正
 
 if __name__ == "__main__":
     argment_parser()
@@ -233,4 +303,5 @@ if __name__ == "__main__":
     block = select_block(joined_blocks)
     text = convert_blocks_tostr(block)
     print(text)
-    tweet(text)
+    if do_tweet:
+        tweet(text)
